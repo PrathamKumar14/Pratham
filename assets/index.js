e454c1b19997fbe2a70d69532360048b104de708
Pratham kumar <prathamsolera@gmail.com>
2020-10-08 19:08:47 +0530

    Smooth timeline expand

--- assets/index.js
+++ assets/index.js
@@ -40,11 +40,11 @@ var isOpen = false;
 
 function expandTimeline() {
   if (!isOpen) {
-    document.getElementById("list-wrapper").style.height = "auto";
+    document.getElementById("list-wrapper").style.maxHeight = "2200px";
     document.getElementById("expand-btn").innerHTML = "COLLAPSE";
     isOpen = true;
   } else {
-    document.getElementById("list-wrapper").style.height = "94px";
+    document.getElementById("list-wrapper").style.maxHeight = "94px";
     document.getElementById("expand-btn").innerHTML = "EXPAND";
     isOpen = false;
   }
